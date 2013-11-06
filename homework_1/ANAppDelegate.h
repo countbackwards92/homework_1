7fad562e9fedff192c9a201e39db1fe883adf1df
ntrb <antiquehighheel@gmail.com>
2013-11-06 17:59:44 +0400

    Initial Commit

---
+++ homework_1/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  homework_1
+//
+//  Created by Администратор on 11/6/13.
+//  Copyright (c) 2013 MSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ homework_1Tests/homework_1Tests.m
@@ -0,0 +1,34 @@
+//
+//  homework_1Tests.m
+//  homework_1Tests
+//
+//  Created by Администратор on 11/6/13.
+//  Copyright (c) 2013 MSU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface homework_1Tests : XCTestCase
+
+@end
+
+@implementation homework_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
